dc38209c80100b88f8091d008af1cc49af0b03ff
AlexanderDmitriev <alex9110077@gmail.com>
2022-06-11 20:56:20 +0300

    Start

---
+++ contacts.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const contactsPath = null;
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
